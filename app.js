d595823856897ae64a3908def0de6cca53540b60
Rohan Mitra <rohanmitra2420@gmail.com>
2023-06-12 21:49:19 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const morgan = require('morgan');
+const foodItems = require('./foodItems.js');
+
+// express app
+const app = express();
+
+//Listen to requests
+app.listen(3000);
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  const foodItems=require("./foodItems.js");
+  res.render('HomePage', { title: 'Home',foodItems});
+});
+
+app.get('/SignUp', (req, res) => {
+  res.render('SignUp', { title: 'Sign Up'});
+});
+
+app.post('/SignUp', (req, res) => {
+  res.redirect('/Menu');
+});
+
+app.get('/Login', (req, res) => {
+  res.render('Login', { title: 'Login'});
+});
+
+app.post('/Login', (req, res) => {
+  res.redirect('/Menu');
+});
+
+app.get('/Menu', (req, res) => {
+  const foodItems=require("./foodItems.js");
+  // console.log(req.query);
+  let filteredFoods=[];
+  const SearchQuery=req.query.name;
+  if (SearchQuery==undefined){
+    filteredFoods=foodItems;
+  }else{
+    filteredFoods = foodItems.filter(item => {
+      return item.name.toLowerCase().includes(SearchQuery.toLowerCase());
+    });
+  }
+  res.render('Menu', { title: 'Menu',foodItems:filteredFoods});
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
